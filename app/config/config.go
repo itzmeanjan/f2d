07e8378a8e49bac67b61357615f4b0df0aad4086
Anjan Roy <anjanroy@yandex.com>
2021-02-20 12:25:29 +0530

    helpful methods to be used when connecting to db

--- app/config/config.go
+++ app/config/config.go
@@ -15,3 +15,28 @@ func Read(file string) error {
 func Get(key string) string {
 	return viper.GetString(key)
 }
+
+// GetDbUser - Connection to be made as User
+func GetDbUser() string {
+	return Get("DB_USER")
+}
+
+// GetDbPassword - Password to be used when connecting to database
+func GetDbPassword() string {
+	return Get("DB_PASSWORD")
+}
+
+// GetDbHost - Database running on host, where to connection attempt to be made
+func GetDbHost() string {
+	return Get("DB_HOST")
+}
+
+// GetDbPort - Database port to connect to
+func GetDbPort() string {
+	return Get("DB_PORT")
+}
+
+// GetDbName - Database to connect to
+func GetDbName() string {
+	return Get("DB_NAME")
+}
