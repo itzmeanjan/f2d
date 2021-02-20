3ffdbaa771f08e6bfc837bbd2d9bcb95fe27ff2c
Anjan Roy <anjanroy@yandex.com>
2021-02-20 10:17:58 +0530

    db schema for tasks

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- This is required for generating UUID in PostgreSQL
+create extension pgcrypto;
+
+create table tasks (
+    id uuid default gen_random_uuid() primary key,
+    client char(42) not null,
+    startBlock bigint not null,
+    contract char(66),
+    topic0 char(66),
+    topic1 char(66),
+    topic2 char(66),
+    topic3 char(66),
+    ts timestamp not null
+);
