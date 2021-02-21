3f94f052d1a0dafbc819d995a1660ef1d333245a
Anjan Roy <anjanroy@yandex.com>
2021-02-21 11:15:59 +0530

    remove all events emitted in a block

--- app/db/event/event.go
+++ app/db/event/event.go
@@ -32,3 +32,22 @@ func Create(db *gorm.DB, events []*schema.EventLogs) bool {
 	return true
 
 }
+
+// Remove - Given block number, attempts to remove all
+// events emitted in that block
+func Remove(db *gorm.DB, blockNumber uint64) bool {
+
+	if err := db.Transaction(func(tx *gorm.DB) error {
+
+		return tx.Where("blocknumber = ?", blockNumber).Delete(&schema.EventLogs{}).Error
+
+	}); err != nil {
+
+		log.Printf("[❗️] Failed to remove event log(s) for block `%d` : %s\n", blockNumber, err.Error())
+		return false
+
+	}
+
+	return true
+
+}
