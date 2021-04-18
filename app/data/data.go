aec83a70cb46638769ad85f5a58167e9bf3c3904
Anjan Roy <anjanroy@yandex.com>
2021-04-18 07:58:30 +0530

    removed not required structs

--- app/data/data.go
+++ app/data/data.go
@@ -71,35 +71,3 @@ func (r *Resources) Release() error {
 	return nil
 
 }
-
-// EtteSubscriptionRequest - Subscribe to event(s) of interest, emitted by `ette`
-type EtteSubscriptionRequest struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	APIKey string `json:"apiKey"`
-}
-
-// EtteSubscriptionResponse - Event subscription/ unsubscription response from `ette`
-type EtteSubscriptionResponse struct {
-	Code    uint   `json:"code"`
-	Message string `json:"msg"`
-}
-
-// EtteBlock - Newly mined block to be received in this form, from `ette`
-type EtteBlock struct {
-	Hash                string  `json:"hash"`
-	Number              uint64  `json:"number"`
-	Time                uint64  `json:"time"`
-	ParentHash          string  `json:"parentHash"`
-	Difficulty          string  `json:"difficulty"`
-	GasUsed             uint64  `json:"gasUsed"`
-	GasLimit            uint64  `json:"gasLimit"`
-	Nonce               string  `json:"nonce"`
-	Miner               string  `json:"miner"`
-	Size                float64 `json:"size"`
-	StateRootHash       string  `json:"stateRootHash"`
-	UncleHash           string  `json:"uncleHash"`
-	TransactionRootHash string  `json:"txRootHash"`
-	ReceiptRootHash     string  `json:"receiptRootHash"`
-	ExtraData           string  `json:"extraData"`
-}
